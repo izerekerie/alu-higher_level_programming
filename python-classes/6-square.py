a62b9ed7a53bf2443942c4cab0e4989cf43b2080
izerekerie <izerekerie@gmail.com>
2024-03-15 21:34:43 +0200

    fix space

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -69,7 +69,8 @@ class Square:
         Raises:
             TypeError: If position is not a tuple of 2 positive integers.
         """
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
+        if not isinstance(value, tuple) 
+               or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
             raise TypeError(
                 "position must be a tuple of 2 positive integers"
                 )
